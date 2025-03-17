a3bd8ed6b7d05fd59773079862f43a58b4b1be17
Suyash Dahake <59247598+suyash0102@users.noreply.github.com>
2025-03-17 16:09:07 +0530

    Update drop_down_search_form_field.dart

--- lib/src/widgets/drop_down_search_form_field.dart
+++ lib/src/widgets/drop_down_search_form_field.dart
@@ -95,7 +95,7 @@ class DropDownSearchFormField<T> extends FormField<String> {
                 textFieldConfiguration: textFieldConfiguration.copyWith(
                   decoration: textFieldConfiguration.decoration.copyWith(
                       errorText: state.errorText,
-                      suffixIcon: const Icon(Icons.arrow_drop_down)),
+                      ),
                   onChanged: (text) {
                     state.didChange(text);
                     textFieldConfiguration.onChanged?.call(text);
